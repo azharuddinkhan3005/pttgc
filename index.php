65797d46ecbced46d98233a1447f59f284245dd8
azharuddinkhan3005 <azharuddin.khan3005@gmail.com>
2018-10-16 16:37:13 +0530

    updated index.php

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-  echo "<h1>This is PTTGC</h1>";
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+/*use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);*/
+
+echo "<h1>TEST</h1>";
